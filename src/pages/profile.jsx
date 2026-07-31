38da3703a05ad030e9f209d80f263ea14c7d01a7
Bruhanova Olga <bruhanovaolga940@gmail.com>
2026-07-31 07:07:27 +0700

    add import Navigate to Profile.jsx

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.jsx";
 import { useFav } from "../context/FavContext.jsx";
 import ClubCard   from "../assets/components/club/ClubCard.jsx";
